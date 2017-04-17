4955683ecc757599758e4cc245c7be36b5767612
DESKTOP-Q07AKA5\F.D.Kang <kangfengde962@163.com>
2017-04-17 13:47:48 +0800

    创建展示分支

---
+++ resources/common/js/mouse.js
@@ -0,0 +1,300 @@
+var Demos = [];
+var FormDesigner = {};
+FormDesigner.designElement = [];
+
+var mouseOffset = null;
+var iMouseDown = false;
+var lMouseState = false;
+var dragObject = null;
+
+var DragDrops = [];
+var curTarget = null;
+var lastTarget = null;
+var dragHelper = null;
+var tempDiv = null;
+var rootParent = null;
+var rootSibling = null;
+
+//选择的表单可视化元素
+var formFieldSelector = [];
+
+//允许输出日志
+var allowPrintLog = false;
+
+var formShowViewPostion = null;
+
+
+function writeHistory(object, message) {
+    if (!object || !object.parentNode || !object.parentNode.getAttribute)
+        return;
+    if(!allowPrintLog)
+        return;
+    var historyDiv = object.parentNode.getAttribute('history') || document.getElementById('formConsole');
+    if (historyDiv) {
+        /*historyDiv = document.getElementById(historyDiv);*/
+        historyDiv.appendChild(document.createTextNode(object.id + ': ' + message));
+        historyDiv.appendChild(document.createElement('BR'));
+
+        historyDiv.scrollTop += 50;
+    }
+}
+
+var mouseInForm = false;
+
+//鼠标移动
+function mouseMove(ev){
+    ev = ev || window.event;
+    var target = ev.target || ev.srcElement;
+    var mousePos = mouseCoords(ev);
+    mouseInForm = false;
+    /*if(target.className.indexOf('form-designer-show-parent') >= 0){
+        mouseInForm = true;
+    }else{
+        mouseInForm = false;
+    }*/
+    if(dragObject){
+        for (var i = 0; i < dragHelper.childNodes.length; i++)
+            dragHelper.removeChild(dragHelper.childNodes[i]);
+        var cn = dragObject.cloneNode(true);
+        if(cn.className.indexOf('form-designer-show-field') >= 0){
+            formFieldSelectorCancel(dragObject);
+            cn.onclick = function(){
+                formFieldCopyClick(cn);
+            };
+            formFieldSelectorSelect(cn);
+        }
+        dragHelper.appendChild(cn);
+        dragHelper.style.display = 'block';
+        dragHelper.style.position = 'absolute';
+        dragHelper.style.top      = (mousePos.y - mouseOffset.y) + 'px';
+        dragHelper.style.left     = (mousePos.x - mouseOffset.x) + 'px';
+        /*return false;*/
+        if(iMouseDown){//鼠标按下状态
+            mouseInFormFn(mousePos);
+            var f = document.getElementById('formShowViewId');
+            if(mouseInForm){
+                if(!findActiveChildren(f)){
+                    var formFieldDiv = document.createElement("div");
+                    formFieldDiv.id = id;
+                    formFieldDiv.className = 'form-designer-show-field form-designer-show-field-active';
+                    formFieldDiv.onclick = function(){
+                        formFieldClick(formFieldDiv);
+                    };
+                    f.appendChild(formFieldDiv);
+                }
+            }else{
+                delActiveChildren(f);
+            }
+        }
+    }
+    /*if(iMouseDown && !lMouseState){
+        debugger;
+    }*/
+    /*console.log(lMouseState);
+    console.log(iMouseDown);*/
+    /*lMouseState = iMouseDown;*/
+}
+
+function delActiveChildren(e){
+    var child = findActiveChildren(e);
+    if(child){
+        e.removeChild(child);
+    }
+}
+
+function findActiveChildren(e){
+    var child = null;
+    if(e.childElementCount > 0){
+        e.childNodes.forEach(function(node, index, nodeList){
+            if(node.className.indexOf('form-designer-show-field-active') >= 0){
+                child = node;
+            }
+        });
+    }
+    return child;
+}
+
+function mouseUp(ev) {
+    /*if (curTarget) {
+        writeHistory(curTarget, 'Mouse Up Fired');
+
+        dragHelper.style.display = 'none';
+        if (curTarget.style.display == 'none') {
+            if (rootSibling) {
+                rootParent.insertBefore(curTarget, rootSibling);
+            } else {
+                rootParent.appendChild(curTarget);
+            }
+        }
+        curTarget.style.display = '';
+        curTarget.style.visibility = 'visible';
+    }*/
+    if(!mouseInForm){
+        dragHelper.style.display = 'none';
+    }
+    curTarget = null;
+    dragObject = null;
+    iMouseDown = false;
+}
+
+function formFieldSelectorSelect(item){
+    if(item.className.indexOf(' form-designer-show-field-active') < 0){
+        item.className += ' form-designer-show-field-active';
+        item.firstChild.style.display = 'block';
+    }
+    formFieldSelectorCancelAll();
+    formFieldSelector.push(item);
+}
+
+function formFieldSelectorCancelAll(){
+    if(formFieldSelector.length > 0){
+        for(var i in formFieldSelector){
+            formFieldSelectorCancel(formFieldSelector[i]);
+        }
+    }
+}
+
+function formFieldSelectorCancel(item){
+    item.className = item.className.replace(' form-designer-show-field-active', '');
+    item.firstChild.style.display = 'none';
+    for(var i in formFieldSelector){
+        if(formFieldSelector[i] == item){
+            formFieldSelector.splice(i, 1);
+        }
+    }
+}
+
+/*function formFieldSelectorCancel(ev){return;
+    if(formFieldSelector.length > 0){
+        var x=event.clientX;
+        var y=event.clientY;
+        var selector = formFieldSelector[0];
+        var pos = getPosition(selector);
+        var divx1 = pos.x;
+        var divy1 = pos.y;
+        var divx2 = pos.x + selector.offsetWidth;
+        var divy2 = pos.y + selector.offsetHeight;
+        if( x < divx1 || x > divx2 || y < divy1 || y > divy2){
+            formFieldSelectorCancelFn(selector);
+        }
+    }
+}*/
+
+function mouseDown(ev) {
+    ev = ev || window.event;
+    var target = ev.target || ev.srcElement;
+
+    iMouseDown = true;
+    if (Demos[0] || Demos[4]) {
+        if (lastTarget) {
+            writeHistory(lastTarget, 'Mouse Down Fired');
+        }
+    }
+    if (target.onmousedown || target.getAttribute('DragObj')) {
+        return false;
+    }
+}
+
+function getPosition(e) {
+    var left = 0;
+    var top = 0;
+    while (e.offsetParent) {
+        left += e.offsetLeft + (e.currentStyle ? (parseInt(e.currentStyle.borderLeftWidth)).NaN0() : 0);
+        top += e.offsetTop + (e.currentStyle ? (parseInt(e.currentStyle.borderTopWidth)).NaN0() : 0);
+        e = e.offsetParent;
+    }
+
+    left += e.offsetLeft + (e.currentStyle ? (parseInt(e.currentStyle.borderLeftWidth)).NaN0() : 0);
+    top += e.offsetTop + (e.currentStyle ? (parseInt(e.currentStyle.borderTopWidth)).NaN0() : 0);
+
+    return {
+        x: left,
+        y: top
+    };
+}
+
+function mouseInFormFn(mousePos){
+    var xMin = formShowViewPostion.x;
+    var xMax = formShowViewPostion.x + formShowViewPostion.width;
+    var yMin = formShowViewPostion.y;
+    var yMax = formShowViewPostion.y + formShowViewPostion.height;
+    var xIn = xMin <= mousePos.x && mousePos.x <= xMax;
+    var yIn = yMin <= mousePos.y && mousePos.y <= yMax;
+    mouseInForm = (xIn && yIn);
+}
+
+function getPositionAndSize(e) {
+    var ps = getPosition(e);
+    ps.width = e.offsetWidth;
+    ps.height = e.offsetHeight;
+    return ps;
+}
+
+function mouseCoords(ev) {
+    if (ev.pageX || ev.pageY) {
+        return {
+            x: ev.pageX,
+            y: ev.pageY
+        };
+    }
+    return {
+        x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
+        y: ev.clientY + document.body.scrollTop - document.body.clientTop
+    };
+}
+
+var getMouseOffset = function(target, ev){
+    ev = ev || window.event;
+    var docPos    = getPosition(target);
+    var mousePos  = mouseCoords(ev);
+    return {x:mousePos.x - docPos.x, y:mousePos.y - docPos.y};
+}
+
+var makeDraggable = function(item){
+    if(!item) return;
+    item.onmousedown = function(ev){
+        dragObject  = this;
+        mouseOffset = getMouseOffset(this, ev);
+        return false;
+    }
+}
+
+var createDragContainer = function(){
+    /*
+    Create a new "Container Instance" so that items from one "Set" can not
+    be dragged into items from another "Set"
+     */
+    var cDrag = DragDrops.length;
+    DragDrops[cDrag] = [];
+
+    /*
+    Each item passed to this function should be a "container".  Store each
+    of these items in our current container
+     */
+    for (var i = 0; i < arguments.length; i++) {
+        var cObj = arguments[i];
+        DragDrops[cDrag].push(cObj);
+        cObj.setAttribute('DropObj', cDrag);
+
+        /*
+        Every top level item in these containers should be draggable.  Do this
+        by setting the DragObj attribute on each item and then later checking
+        this attribute in the mouseMove function
+         */
+        for (var j = 0; j < cObj.childNodes.length; j++) {
+
+            // Firefox puts in lots of #text nodes...skip these
+            if (cObj.childNodes[j].nodeName == '#text')
+                continue;
+
+            cObj.childNodes[j].setAttribute('DragObj', cDrag);
+        }
+    }
+}
+
+var createDragHelper = function(){
+    // Create our helper object that will show the item while dragging
+    dragHelper = document.createElement('div');
+    dragHelper.style.cssText = 'position:absolute;display:none;';
+    document.body.appendChild(dragHelper);
+}
---
+++ resources/form/desigener.js
@@ -0,0 +1,252 @@
+/***********************************************
+ *  title: 表单设计器
+ *  autor：F.D.Kang
+ *  time: 2017/04/13
+ *  desc: 基于ext做的表单设计
+ *  用途：用的demo演示
+ *
+ ***********************************************/
+ //Ext.namespace( 'Designer');
+ //创建表单设计的panel
+/* var Designer = Ext.create('Ext.panel.Panel', {
+        renderTo: document.body,
+        title: '表单设计器demo',
+        layout: 'fit',
+        tbar: [
+          { xtype: 'button', text: '新增' }
+        ]
+    });*/
+
+var formFieldStore = Ext.create('Ext.data.Store', {
+    data: [
+        { class:'form-icon-textfield-32', caption:'单行文本' },
+        { class:'form-icon-textarea-32', caption:'多行文本' },
+        { class:'form-icon-number-32', caption:'数字' },
+        { class:'form-icon-radio-32', caption:'单选框' },
+        { class:'form-icon-multiselect-32', caption:'多选框' },
+        { class:'form-icon-singleselect-32', caption:'下拉框' },
+        { class:'form-icon-image-32', caption:'上传图片' },
+        { class:'form-icon-datepicker-32', caption:'日期时间' },
+        { class:'form-icon-checkbox-32', caption:'开关' },
+        { class:'form-icon-label-32', caption:'文字说明' },
+        { class:'form-icon-hidden-32', caption:'隐藏框' },
+        { class:'form-icon-imagegroup-32', caption:'图片组' }
+    ],
+    fields: ['class', 'caption']
+});
+
+
+
+var formFieldViewId = 'formFieldViewId';
+
+var formFieldTpl = new Ext.XTemplate(
+    '<div class="form-ctltop" id="formFieldViewId"><tpl for=".">',
+        '<div form-category="base" class="ui-draggable-handle" form-field-view-id="formFieldViewId">',
+          '<span class="form-label-img {class}"></span>',
+          '<span class="form-label-name">{caption}</span>',
+        '</div>',
+    '</tpl></div>'
+);
+
+var formFieldView = Ext.create('Ext.view.View', {
+    store: formFieldStore,
+    tpl: formFieldTpl,
+    itemSelector: 'div.ui-draggable-handle',
+    emptyText: '没有控件'/*,
+    listeners: {
+        containermousedown : function(tem, e, eOpts){
+            debugger;
+        },
+        containermouseout : function(tem, e, eOpts){//鼠标离开
+            debugger;
+        },
+        containermouseover : function(tem, e, eOpts){//鼠标进入
+            debugger;
+        },
+        containermouseup : function(tem, e, eOpts){//debugger;
+            debugger;
+        }
+    }*/
+});
+
+/*var formPage = Ext.create('Ext.panel.Panel', {
+        width: 700,
+        autoWidth: true,
+        autoHeight: true,
+        height: 100,
+        html: '<center><div style="width:700px;height:100%;"></div></center>',
+        tbar: [
+          { xtype: 'button', text: '新增' }
+        ]
+    });*/
+
+var Designer = Ext.create('Ext.container.Viewport', {
+    layout: 'border',
+    items: [{
+        region: 'north',
+        html: '<h1 class="x-panel-header">表单设计器demo</h1>',
+        border: false,
+        margin: '0 0 5 0'
+    }, {
+        region: 'west',
+        collapsible: true,
+        title: '表单元素',
+        split: true,
+        width: 250,
+        items: formFieldView
+        // could use a TreePanel or AccordionLayout for navigational items
+    }, {
+        region: 'south',
+        title: '控制台',
+        collapsible: true,
+        /*overflowY: 'auto',*/
+        html: '<div id="formConsole" class="form-designer-console-history">Information goes here</div>',
+        split: true,
+        height: 100,
+        /*margin: '-5 0 0 0',*/
+        minHeight: 100,
+        tools: [{
+            type:'print',
+            tooltip: '是否开启日志输出',
+            handler: function(event, toolEl, panelHeader) {
+                allowPrintLog = allowPrintLog ? false : true;
+            }
+        },{
+            type:'refresh',
+            tooltip: '清除日志信息',
+            handler: function(event, toolEl, panelHeader) {
+                this.up().up().update('<div id="formConsole" class="form-designer-console-history">Information goes here</div>');
+            }
+        }]
+    }, {
+        region: 'east',
+        title: '属性',
+        titleAlign: 'right',
+        collapsible: true,
+        collapseAlign: 'left',
+        split: true,
+        width: 150,
+        listeners: {
+            afterlayout: function(pt, layout, eOpts){
+                this.tools[0].el.dom.style.left = 0;
+            }
+        }
+    }, {
+        region: 'center',
+        html: '<center><div class="form-designer-show-parent" id="formShowViewId"></div></center>'
+        /*items: formPage*/
+    }]
+});
+
+var getFormShowViewPosition = function(){
+    var f = document.getElementById('formShowViewId');
+    formShowViewPostion = getPositionAndSize(f);
+}
+
+var setCursorMove = function(el){
+    if(typeof el == 'string'){
+        el = document.getElementById(el);
+    }/* else if(!(el instanceof HTMLElement)){debugger;
+        el.parentElement.style = 'move';
+    }*/
+    if(el.childElementCount > 0){
+        for (var i = el.childNodes.length - 1; i >= 0; i--) {
+            setCursorMove(el.childNodes[i]);
+        }
+    }
+    if(el.className == 'design-ico-copy'
+        || el.className == 'design-ico-delete'){
+        return;
+    }
+    el.style.cursor = 'move';
+}
+
+var getFieldName = function(id){
+    if(id == 'testShow1'){
+        return 'Ext.form.field.Text';
+    }else{
+        return 'Ext.form.field.Date';
+    }
+    /*var num = Math.round(Math.random() * 2);
+    if(num === 1){
+        return 'Ext.form.field.Text';
+    }else if(num === 2){
+        return 'Ext.form.field.ComboBox';
+    }else{
+        return 'Ext.form.field.Text';
+    }*/
+}
+
+var formFieldClick = function(item){
+    if(formFieldSelector.length == 1
+        && formFieldSelector[0] == item){
+        return false;
+    }
+    formFieldSelectorCancelAll();
+    formFieldSelectorSelect(item);
+}
+
+var formFieldCopyClick = function(item){
+    formFieldClick(item);
+}
+var formFieldDelClick = function(item){
+    formFieldClick(item);
+}
+
+var createFormFieldDiv = function(id){
+    var f = document.getElementById('formShowViewId');
+    var formFieldDiv = document.createElement("div");
+    formFieldDiv.id = id;
+    formFieldDiv.className = 'form-designer-show-field';
+    formFieldDiv.onclick = function(){
+        formFieldClick(formFieldDiv);
+    };
+    f.appendChild(formFieldDiv);
+    var formFieldCopyDelSpan = document.createElement("span");
+    formFieldCopyDelSpan.className = 'design-copy-delete';
+    var copyI = document.createElement("i");
+    copyI.className = 'design-ico-copy';
+    copyI.onclick = function(){
+        formFieldCopyClick(formFieldDiv);
+    };
+    var delI = document.createElement("i");
+    delI.className = 'design-ico-delete';
+    delI.onclick = function(){
+        formFieldDelClick(formFieldDiv);
+    }
+    formFieldCopyDelSpan.appendChild(copyI);
+    formFieldCopyDelSpan.appendChild(delI);
+    //formFieldCopyDelSpan.innerHTML = '<i class="design-ico-copy" ours-e-click="cloneControl"></i><i class="design-ico-delete" ours-e-click="deleteControl"></i>';
+    formFieldDiv.appendChild(formFieldCopyDelSpan);
+}
+
+var createFormField = function(id){
+    createFormFieldDiv(id);
+    Ext.create(getFieldName(id), {
+        name: 'name',
+        fieldLabel: 'Name1',
+        disabled: true,
+        disabledCls: 'form-readonly',
+        renderTo: id/*,
+        readOnly: true*/
+    }).getEl().setStyle('z-index','0');
+    setCursorMove(id);
+    makeDraggable(document.getElementById(id));
+}
+
+createFormField('testShow1');
+createFormField('testShow2');
+
+var makeFormFieldViewDraggable = function(){
+    var formFieldDiv = document.getElementById('formFieldViewId');
+    if(formFieldDiv.childElementCount > 0){
+        formFieldDiv.childNodes.forEach(function(node, index, nodeList){
+            makeDraggable(node);
+        });
+    }
+}
+makeFormFieldViewDraggable();
+
+createDragContainer(document.getElementById('formFieldViewId'), document.getElementById('formShowViewId'));
+createDragHelper();
+getFormShowViewPosition();
